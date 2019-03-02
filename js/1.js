ba229ac37e49fb749c66967adfc29b89a4a9eddd
cương nguyễn <cuong1997565@gmail.com>
2019-03-02 21:17:15 +0700

    template

---
+++ js/1.js
@@ -0,0 +1,41 @@
+﻿$(function() {
+
+		$('.n0,.bottoms').click(function(event) {
+			/* Act on the event */
+			$('body').animate({scrollTop: 0},1000,"easeInOutExpo");
+			return false;
+		});
+
+		$('.n1').click(function(event) {
+			/* Act on the event */
+			$('body').animate({scrollTop: $('#pfo').offset().top},1000, "easeInOutExpo");
+			return false;
+		});
+
+		$('.n2').click(function(event) {
+			/* Act on the event */
+			$('body').animate({scrollTop: $('#ab').offset().top},1000, "easeInOutExpo");
+		});
+
+		$('.n3').click(function(event) {
+			/* Act on the event */
+			$('body').animate({scrollTop: $('#ct').offset().top},1000, "easeInOutExpo");
+		});
+
+		$('.fa.fa-chevron-circle-down').click(function(event) {
+			/* Act on the event */
+			$('body').animate({scrollTop: $('.footer').offset().top},1500, "easeInOutExpo");
+		});
+
+		$(window).scroll(function(event) {
+			/* Act on the event */
+			local = $('body').scrollTop();
+			console.log(local);
+
+			if (local>50) {
+				$('nav.navbar.navbar-default').addClass('hides');
+			} else {
+				$('nav.navbar.navbar-default').removeClass('hides');
+			}
+	});
+});
